ed500981708cee69789cbbfc0949c86e23959a09
manugaco <manu.garcia.corbi@gmail.com>
2022-03-03 09:45:14 +0100

    UPDATE query ranges

--- SMI_1_users/build/queries/SMI_query_users.sql
+++ SMI_1_users/build/queries/SMI_query_users.sql
@@ -1,11 +1,11 @@
 select *
 from {schema}.smi_users sut
 where 
-sut."smi_int_followers" < 30
-AND sut."smi_int_followers" > 5
-AND sut."smi_int_friends" < 30
-AND sut."smi_int_friends" > 5
-AND 
+--sut."smi_int_followers" < 30
+--AND sut."smi_int_followers" > 5
+--AND sut."smi_int_friends" < 30
+--AND sut."smi_int_friends" > 5
+--AND 
 sut."smi_bool_protected" = False
 AND (sut."smi_str_lastlookup" IS NULL
 OR TO_DATE(sut."smi_str_lastlookup", 'YYYY-MM-DD') < CURRENT_DATE);
